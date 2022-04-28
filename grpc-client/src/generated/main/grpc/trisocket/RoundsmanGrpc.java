6a854cf0650fcd0db8065ee795085a6db1699414
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2022-04-28 12:47:31 +0300

    update dependencies

--- grpc-client/src/generated/main/grpc/trisocket/RoundsmanGrpc.java
+++ grpc-client/src/generated/main/grpc/trisocket/RoundsmanGrpc.java
@@ -5,7 +5,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.43.2)",
+    value = "by gRPC proto compiler (version 1.46.0)",
     comments = "Source: service.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class RoundsmanGrpc {
